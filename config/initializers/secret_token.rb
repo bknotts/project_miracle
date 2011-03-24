0379910301bc72ffef7dcc725e6bdea3d741c13a
Brennan Knotts <rbrennanknotts@gmail.com>
2011-03-24 16:08:46 -0400

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+ProjectMiracle::Application.config.secret_token = '1c8507f8bd54a6667b5865a8a3eed6bf447fecf883be49451fe71acab901bddc787e77da892d6b650d8a19918901a8e6d9e07b381bdddb826e40ae6ae4ea45bc'
