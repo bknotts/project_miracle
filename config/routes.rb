45638d3161096f849d0e6455282453266d68b0ea
Brennan Knotts <rbrennanknotts@gmail.com>
2011-03-25 11:14:08 -0400

    Updated routes and created integration tests for home and about pages

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 ProjectMiracle::Application.routes.draw do
-  get "pages/home"
-
-  get "pages/about"
+  match '/about', :to => 'pages#about'
+  root :to => 'pages#home'
+end
 
   # The priority is based upon order of creation:
   # first created -> highest priority.
@@ -59,4 +59,3 @@ ProjectMiracle::Application.routes.draw do
   # This is a legacy wild controller route that's not recommended for RESTful applications.
   # Note: This route will make all actions in every controller accessible via GET requests.
   # match ':controller(/:action(/:id(.:format)))'
-end
