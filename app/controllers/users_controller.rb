8e9d31d7d7e1f69b4971aa89c0a4fa54358bb397
Brennan Knotts <rbrennanknotts@gmail.com>
2011-03-29 09:00:24 -0400

    Added successful signup redirect to user page

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -9,4 +9,14 @@ class UsersController < ApplicationController
     @user = User.new
     @title = "Sign Up"
   end
+  
+  def create
+    @user = User.new(params[:user])
+    if @user.save
+      redirect_to @user
+    else
+      @title = "Sign Up"
+      render 'new'
+    end
+  end
 end
