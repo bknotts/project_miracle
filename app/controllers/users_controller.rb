3ea55de0666921d32f7425e47f43fbd388820279
Brennan Knotts <rbrennanknotts@gmail.com>
2011-03-30 19:41:41 -0400

    Added All Users page with pagination

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,5 @@
 class UsersController < ApplicationController
-  before_filter :authenticate, :only => [:edit, :update]
+  before_filter :authenticate, :only => [:edit, :update, :index]
   before_filter :correct_user, :only => [:edit, :update]
   
   def show
@@ -40,6 +40,11 @@ class UsersController < ApplicationController
       @title = "Edit Profile"
       render 'edit'
     end
+  end
+  
+  def index
+    @title = "All users"
+    @users = User.paginate(:page => params[:page])
   end
   
   private
