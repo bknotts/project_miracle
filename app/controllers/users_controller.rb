c09255e84949a64c8525df26c8eeea0bb9fd89f2
Brennan Knotts <rbrennanknotts@gmail.com>
2011-03-30 15:10:12 -0400

    Added filters to prevent non-signed-in and wrong users from accessing certain pages and actions

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,6 @@
 class UsersController < ApplicationController
+  before_filter :authenticate, :only => [:edit, :update]
+  before_filter :correct_user, :only => [:edit, :update]
   
   def show
     @user = User.find(params[:id])
@@ -23,4 +25,32 @@ class UsersController < ApplicationController
       render 'new'
     end
   end
+  
+  def edit
+    @user = User.find(params[:id])
+    @title = "Edit Profile"
+  end
+  
+  def update
+    @user = User.find(params[:id])
+    if @user.update_attributes(params[:user])
+      flash[:success] = "Successfully Updated"
+      redirect_to @user
+    else
+      @title = "Edit Profile"
+      render 'edit'
+    end
+  end
+  
+  private
+  
+    def authenticate
+      deny_access unless signed_in?
+    end
+    
+    def correct_user
+      @user = User.find(params[:id])
+      redirect_to(user_path(@user)) unless current_user?(@user)
+    end
+  
 end
